6ae072d50fc7f5b393b7a68160f86e772ac25870
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-22 18:58:35 +0200

    Initial version of app logic
    - 3 types of which the user is able to select - soft/medium/hard
    - timer for each of the types with live update
    - start/stop button with haptic feature
    - UI Layout constraints to support portrait/landscape mode + different devices

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,11 +9,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var softEggImageView: UIImageView!
+    @IBOutlet weak var mediumEggImageView: UIImageView!
+    @IBOutlet weak var hardEggImageView: UIImageView!
+    
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    var timer:Timer?
+    var totalTime:Int = 0
+    var totalTimeSelected:Int = 0
+    let totalTimeSoftEgg = 180
+    let totalTimeMediumEgg = 300
+    let totalTimeHardEgg = 420
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.totalTime = totalTimeSoftEgg
+        self.totalTimeSelected = totalTimeSoftEgg
     }
 
-
+    @IBAction func eggSegment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            self.totalTime = totalTimeSoftEgg
+            self.totalTimeSelected = totalTimeSoftEgg
+            self.timerLabel.text = self.timeFormatted(self.totalTime)
+            self.softEggImageView.alpha = 1
+            self.mediumEggImageView.alpha = 0.5
+            self.hardEggImageView.alpha = 0.5
+        } else if sender.selectedSegmentIndex == 1 {
+            self.totalTime = totalTimeMediumEgg
+            self.totalTimeSelected = totalTimeMediumEgg
+            self.timerLabel.text = self.timeFormatted(self.totalTime)
+            self.softEggImageView.alpha = 0.5
+            self.mediumEggImageView.alpha = 1
+            self.hardEggImageView.alpha = 0.5
+        } else if sender.selectedSegmentIndex == 2 {
+            self.totalTime = totalTimeHardEgg
+            self.totalTimeSelected = totalTimeHardEgg
+            self.timerLabel.text = self.timeFormatted(self.totalTime)
+            self.softEggImageView.alpha = 0.5
+            self.mediumEggImageView.alpha = 0.5
+            self.hardEggImageView.alpha = 1
+        }
+    }
+    
+    @IBAction func startStopButtonPressed(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Start" {
+            self.startStopButton.setTitle("Stop", for: .normal)
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+            self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        } else if sender.titleLabel?.text == "Stop" {
+            self.startStopButton.setTitle("Start", for: .normal)
+            self.timer?.invalidate()
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+        }
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    @objc func updateTimer() {
+        self.timerLabel.text = self.timeFormatted(self.totalTime)
+        if totalTime != 0 {
+            totalTime -= 1
+            progressView.progress = Float(totalTimeSelected - totalTime) / Float(totalTimeSelected)
+            print(progressView.progress)
+        } else {
+            if let timer = self.timer {
+                timer.invalidate()
+                self.timer = nil
+                UINotificationFeedbackGenerator().notificationOccurred(.success)
+            }
+        }
+    }
+    
 }
 
